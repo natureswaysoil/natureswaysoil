9c85a104fbf3386556c67d345840db5c6183353a
natureswaysoil <natureswaysoil@gmail.com>
2025-08-14 15:44:34 -0400

    Update and rename pages/data.ts to pages/data/products.ts

---
+++ pages/data/products.ts
@@ -0,0 +1,20 @@
+export type Product = { id: string; title: string; price: number; priceId: string; image: string };
+
+export const PRODUCTS: Product[] = [
+  { id: "nws-001", title: "Premium Organic Mix",        price: 19.99, priceId: "price_TODO_nws001", image: "https://m.media-amazon.com/images/I/61ll2EiLAJL._AC_UL320_.jpg" },
+  { id: "nws-002", title: "All-Purpose Garden Blend",    price: 16.99, priceId: "price_TODO_nws002", image: "https://m.media-amazon.com/images/I/615mJs9XccL._AC_UL320_.jpg" },
+  { id: "nws-003", title: "Compost Enriched Potting",    price: 17.99, priceId: "price_TODO_nws003", image: "https://m.media-amazon.com/images/I/510ui3CBLbL._AC_UL320_.jpg" },
+  { id: "nws-004", title: "Biochar Boost Blend",         price: 18.99, priceId: "price_TODO_nws004", image: "https://m.media-amazon.com/images/I/61qsUDP+WuL._AC_UL320_.jpg" },
+  { id: "nws-005", title: "Tomato & Veggie Soil",        price: 14.99, priceId: "price_TODO_nws005", image: "https://m.media-amazon.com/images/I/718tWBNNfkL._AC_UL320_.jpg" },
+  { id: "nws-006", title: "Tomato & Veggie Soil (2-Pack)",price: 27.99, priceId: "price_TODO_nws006", image: "https://m.media-amazon.com/images/I/718tWBNNfkL._AC_UL320_.jpg" },
+  { id: "nws-007", title: "Orchid & Violet Mix",         price: 15.99, priceId: "price_TODO_nws007", image: "https://m.media-amazon.com/images/I/7151rsGhpkL._AC_UL320_.jpg" },
+  { id: "nws-008", title: "Humic & Fulvic Booster",      price: 18.49, priceId: "price_TODO_nws008", image: "https://m.media-amazon.com/images/I/61MSABGkBKL._AC_UL320_.jpg" },
+  { id: "nws-009", title: "Liquid Kelp 32oz",            price: 16.49, priceId: "price_TODO_nws009", image: "https://m.media-amazon.com/images/I/71PYCZfZ2BL._AC_UL320_.jpg" },
+  { id: "nws-010", title: "All-Natural Lawn Food",       price: 21.99, priceId: "price_TODO_nws010", image: "https://m.media-amazon.com/images/I/91D+7LuTsQL._AC_UL320_.jpg" },
+  { id: "nws-011", title: "Seed Starting Mix",           price: 13.99, priceId: "price_TODO_nws011", image: "https://m.media-amazon.com/images/I/61PuOA7jGiL._AC_UL320_.jpg" },
+  { id: "nws-012", title: "Cactus & Succulent Mix",      price: 12.99, priceId: "price_TODO_nws012", image: "https://m.media-amazon.com/images/I/61PK-OTlOdL._AC_UL320_.jpg" },
+  { id: "nws-013", title: "Worm Castings Enriched",      price: 22.99, priceId: "price_TODO_nws013", image: "https://m.media-amazon.com/images/I/61IrwxMbtPL._AC_UL320_.jpg" },
+  { id: "nws-014", title: "Raised Bed Super Soil",       price: 24.99, priceId: "price_TODO_nws014", image: "https://m.media-amazon.com/images/I/81vwGq+i7iL._AC_UL320_.jpg" },
+  { id: "nws-015", title: "Indoor Houseplant Mix",       price: 14.49, priceId: "price_TODO_nws015", image: "https://m.media-amazon.com/images/I/71UwwRFL0rL._AC_UL320_.jpg" },
+  { id: "nws-016", title: "Berry & Flowering Mix",       price: 18.49, priceId: "price_TODO_nws016", image: "https://m.media-amazon.com/images/I/71vja1k7ywL._AC_UL320_.jpg" }
+];
