6d5bad01b448a209ba3cf2285aa8f4c7635e8e5e
natureswaysoil <natureswaysoil@gmail.com>
2025-08-15 18:19:17 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
 // pages/_app.tsx
 import type { AppProps } from "next/app";
-import "../styles/globals.css";
+import HeaderCart from "../components/HeaderCart";
+import "../styles/globals.css"; // if you have it
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      {/* Your site header goes here... */}
+      <div className="sticky top-0 z-30 bg-white/80 backdrop-blur border-b">
+        <div className="max-w-7xl mx-auto h-16 px-6 flex items-center justify-between">
+          <a href="/" className="font-semibold">Nature’s Way Soil</a>
+          <HeaderCart />
+        </div>
+      </div>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
