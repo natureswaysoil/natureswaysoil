c39ca7bef6b2b60221e2b47908c7f71411f72758
natureswaysoil <natureswaysoil@gmail.com>
2025-08-16 12:57:08 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
-import "@/styles/globals.css"; // <-- loads Tailwind
+import "@/styles/globals.css"; // If @ alias isn't set, use: "../styles/globals.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+
