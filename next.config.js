448ee3435936c1393524d7c09813bffe6dcd4873
natureswaysoil <natureswaysoil@gmail.com>
2025-08-17 20:33:04 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,10 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'm.media-amazon.com',
-        pathname: '/images/**',
-      },
-    ],
-    // If you're on an older Next, this also works:
-    // domains: ['m.media-amazon.com'],
-  },
+      { protocol: 'https', hostname: 'm.media-amazon.com' }
+    ]
+  }
 };
-
 module.exports = nextConfig;
